bb4884f4078541396665fefeb519c55650abfe9b
ipadorusa <ipadorusa@gmail.com>
2021-05-09 23:07:04 +0900

    #6.1 Building the Router 까지 클론 코딩

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ProTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div className='movie'>
       <img src={poster} alt={title} title={title} />
       <div className='movie__data'>
         <h3 className='movie__title'>{title}</h3>
         <h5 className='movie__year'>{year}</h5>
+        <ul className='movie__genres'>
+          {genres.map((genre, index) => (
+            <li key={index} className='genres__genre'>
+              {genre}
+            </li>
+          ))}
+        </ul>
         <p className='movie__summary'>{summary}</p>
       </div>
     </div>
@@ -20,5 +28,6 @@ Movie.ProTypes = {
   title: ProTypes.string.isRequired,
   summary: ProTypes.string.isRequired,
   poster: ProTypes.string.isRequired,
+  genres: ProTypes.arrayOf(ProTypes.string).isRequired,
 };
 export default Movie;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About() {
+  return <div>About this page</div>;
+}
+
+export default About;
